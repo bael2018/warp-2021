a711be4eb82b5f75e03e33ca646c099617d473bc
bael <osh.kg1819@gmail.com>
2021-10-08 14:15:01 +0600

    added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ closeBtn.addEventListener('click' , e => {
     e.preventDefault()
     link.classList.add('activeLink')
 })
+
+// login
+
+const login = document.querySelector('.login')
+const login_show = document.querySelector('.login_show')
+
+login.addEventListener('click' , e => {
+    e.preventDefault()
+
+    login_show.classList.toggle('activeSpan')
+})
+
+login_show.addEventListener('click' , e => {
+    e.preventDefault()
+
+    window.open('login.html')
+})
